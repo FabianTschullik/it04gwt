426e49bf0f69806ae450e6e6b8044e0142853c88
Fabian <fabian.tschullik@googlemail.com>
2015-05-22 09:23:39 +0200

    Daten zu Commit davor

---
+++ src/de/hdm/it04/client/MainViewEditor.java
@@ -0,0 +1,65 @@
+package de.hdm.it04.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class MainViewEditor extends Composite {
+	private VerticalPanel vPanel = new VerticalPanel();
+	private VerticalPanel contentPanel;
+	private Label label1;
+	private Label label2;
+	private Image abaugruppeMain;
+	
+	
+	
+	public MainViewEditor() {                         //Constructor
+		initWidget(this.vPanel);
+		//this.vPanel.setBorderWidth(1);
+		
+		MenuViewEditor menu = new MenuViewEditor(this);
+		this.vPanel.add(menu);
+		
+		
+		
+		
+		
+		this.contentPanel = new VerticalPanel();
+		this.vPanel.add(contentPanel);
+		
+		Label label1 = new Label("Hallo Welt!!!!!!!!!!!!!!!!!!!!!!!");
+		this.vPanel.add(label1);
+		
+		Label label2 = new Label("Hallo Welt!!!!!!!!!!!!!!!!!!!!!!!");
+		this.vPanel.add(label2);
+		
+		
+		Image abaugruppeMain = new Image("war/images/baugruppe.jpg");
+		abaugruppeMain.setWidth("600px");
+		this.vPanel.add(abaugruppeMain);
+		
+		
+	}
+		
+		
+		public void openBaugruppeMain() {
+			this.vPanel.clear();
+			BaugruppeMain BaugruppeMain = new BaugruppeMain(this);
+			this.contentPanel.add(BaugruppeMain);
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ src/de/hdm/it04/client/MenuViewEditor.java
@@ -0,0 +1,89 @@
+package de.hdm.it04.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+public class MenuViewEditor extends Composite {
+
+	private HorizontalPanel hPanel = new HorizontalPanel();
+	private MainViewEditor main;
+
+	public MenuViewEditor(MainViewEditor main) {
+
+		initWidget(this.hPanel);
+		this.main = main;
+
+		Button baugruppeBtn1 = new Button("Baugruppe");
+		baugruppeBtn1.addClickHandler(new BaugruppeBtn1ClickHandler());
+		this.hPanel.add(baugruppeBtn1);
+
+		Button BauteilBtn1 = new Button("Bauteil");
+		BauteilBtn1.addClickHandler(new BauteilBtn1ClickHandler());
+		this.hPanel.add(BauteilBtn1);
+
+		Button EnderzeugnisBtn1 = new Button("Enderzeugnis");
+		EnderzeugnisBtn1.addClickHandler(new EnderzeugnisBtn1ClickHandler());
+		this.hPanel.add(EnderzeugnisBtn1);
+
+		Button StuecklisteBtn1 = new Button("Stueckliste");
+		EnderzeugnisBtn1.addClickHandler(new StuecklisteBtn1ClickHandler());
+		this.hPanel.add(StuecklisteBtn1);
+
+		Button ImpressumBtn1 = new Button("Impressum");
+		ImpressumBtn1.addClickHandler(new ImpressumBtn1ClickHandler());
+		this.hPanel.add(ImpressumBtn1);
+
+	}
+
+	private class BaugruppeBtn1ClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			main.openBaugruppeMain();
+
+		}
+
+	}
+
+	private class BauteilBtn1ClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// main.openBauteilMain();
+
+		}
+
+	}
+
+	private class EnderzeugnisBtn1ClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// main.openEnderzeugnisMain();
+
+		}
+
+	}
+
+	private class StuecklisteBtn1ClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// main.openStuecklsiteMain();
+
+		}
+
+	}
+
+	private class ImpressumBtn1ClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// main.openImpressumMain();
+		}
+	}
+
+}
