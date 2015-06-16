40db6636e4713d8e4528b14f3214a033711c222b
MaehlerChristian <cm086@hdm-stuttgart.de>
2015-06-16 11:43:02 +0200

    DbConnection Kommentare eingefügt

--- src/de/hdm/it04/server/db/DbConnection.java
+++ src/de/hdm/it04/server/db/DbConnection.java
@@ -3,20 +3,70 @@ package de.hdm.it04.server.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+ * Verwalten einer Verbindung zur Datenbank.
+ * <p>
+ * <b>Vorteil:</b> Sehr einfacher Verbindungsaufbau zur Datenbank.
+ * <p>
+ * <b>Nachteil:</b> Durch die Singleton-Eigenschaft der Klasse kann nur auf eine
+ * fest vorgegebene Datenbank zugegriffen werden.
+ * <p>
+ * In der Praxis kommen die meisten Anwendungen mit einer einzigen Datenbank
+ * aus. Eine flexiblere Variante für mehrere gleichzeitige
+ * Datenbank-Verbindungen wäre sicherlich leistungsfähiger. Dies würde
+ * allerdings den Rahmen dieses Projekts sprengen bzw. die Software unnötig
+ * verkomplizieren, da dies für diesen Anwendungsfall nicht erforderlich ist.
 
-
+ */
 public class DbConnection {
-
-   
-    
+	
+	/**
+     * Die Klasse DbConnection wird nur einmal instantiiert. Man spricht hierbei
+     * von einem sogenannten <b>Singleton</b>.
+     * <p>
+     * Diese Variable ist durch den Bezeichner <code>static</code> nur einmal
+     * für sämtliche eventuellen Instanzen dieser Klasse vorhanden. Sie
+     * speichert die einzige Instanz dieser Klasse.
+     * Diese Methode bietet einen globen Zugriff auf das Objekt über die instanzoperation.
+     * 
+     * @see BauteilMapper.bauteilMapper()
+     */ 
     private static Connection con = null;
-
-   
-     
+    
+    /**
+     * Die URL, mit deren Hilfe die Datenbank angesprochen wird. In einer
+     * professionellen Applikation würde diese Zeichenkette aus einer
+     * Konfigurationsdatei eingelesen oder über einen Parameter von außen
+     * mitgegeben, um bei einer Veränderung dieser URL nicht die gesamte
+     * Software neu komilieren zu müssen.
+     */
     //private static String googleUrl = "jdbc:google:mysql://it04sms:it04/sms?user=root";
     private static String localUrl = "jdbc:mysql://127.0.0.1:3306/sms?user=root&password=root";
 
-  
+    /**
+     * Diese statische Methode kann aufgrufen werden durch
+     * <code>DbConnection.connection()</code>. Sie stellt die
+     * Singleton-Eigenschaft sicher, indem Sie dafür sorgt, dass nur eine
+     * einzige Instanz von <code>DbConnection</code> existiert.
+     * <p>
+     * 
+     * <b>Fazit:</b> DbConnection sollte nicht mittels <code>new</code>
+     * instantiiert werden, sondern stets durch Aufruf dieser statischen
+     * Methode.
+     * <p>
+     * 
+     * <b>Nachteil:</b> Bei Zusammenbruch der Verbindung zur Datenbank - dies
+     * kann z.B. durch ein unbeabsichtigtes Herunterfahren der Datenbank
+     * ausgelöst werden - wird keine neue Verbindung aufgebaut, so dass die in
+     * einem solchen Fall die gesamte Software neu zu starten ist. In einer
+     * robusten Lösung würde man hier die Klasse dahingehend modifizieren, dass
+     * bei einer nicht mehr funktionsfähigen Verbindung stets versucht würde,
+     * eine neue Verbindung aufzubauen. Dies würde allerdings ebenfalls den
+     * Rahmen dieses Projekts sprengen.
+     * 
+     * @return DAS <code>DbConncetion</code>-Objekt.
+     * @see con
+     */
     public static Connection connection() {
        
         if (con == null) {
@@ -24,7 +74,7 @@ public class DbConnection {
             try {
                
                     
-                    Class.forName("com.mysql.jdbc.Driver");  //f�r local use: "com.mysql.jdbc.Driver" or "com.mysql.jdbc.GoogleDriver" for cloud sql
+                    Class.forName("com.mysql.jdbc.Driver");  //f�r local use: "com.mysql.jdbc.Driver" or "com.mysql.jdbc.GoogleDriver" for cloud sql
                     url = localUrl;
                
                 
