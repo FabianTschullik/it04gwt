7306c64cdac16df248d888cea14b57762f187292
ManuelVoelker <mv045@hdm-stuttgart.de>
2015-06-22 13:30:15 +0200

    Klasse Baugruppe angelegt

---
+++ src/de/hdm/it04/shared/Baugruppe.java
@@ -0,0 +1,13 @@
+package de.hdm.it04.shared;
+
+/**
+ * Die Klasse Baugruppe repräsentiert eine Baugruppe und ist im <code>shared</code> Package.
+ * Da die Klasse von <code>Element</code> und <code>BusinessObject</code> erbt, 
+ * enthält sie auch deren Attribute.
+ */
+@SuppressWarnings("serial")
+public class Baugruppe extends Element {
+		
+
+	}
+
