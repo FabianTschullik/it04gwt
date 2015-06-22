850e6fb51dc5cb740b0e6655579c91623a76dcdc
FabianTschullik <fabian.tschullik@googlemail.com>
2015-06-22 15:56:44 +0200

    tree eingefügt

--- src/de/hdm/it04/client/editor/DataTree.java
+++ src/de/hdm/it04/client/editor/DataTree.java
@@ -2,6 +2,8 @@ package de.hdm.it04.client.editor;
 
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.it04.client.editor.MainViewEditor;
@@ -10,6 +12,7 @@ import de.hdm.it04.client.editor.MainViewEditor;
 public class DataTree extends Composite{
 	
 	private VerticalPanel vPanelTreeTree = new VerticalPanel();
+	private VerticalPanel vPanelTree = new VerticalPanel();
 	
 	private MainViewEditor main;
 	
@@ -23,6 +26,33 @@ public class DataTree extends Composite{
 	
 	Label platzhalterLabel = new Label ("Tree Platzhalter");
 	this.vPanelTreeTree.add(platzhalterLabel);
+	
+	
+	
+	TreeItem root = new TreeItem();
+    root.setText("Baugruppe");
+    
+    	TreeItem sub = new TreeItem();
+    	sub.setText("Unterbaugruppe");
+    
+    		sub.addTextItem("Bauteil 1");
+    		sub.addTextItem("Bauteil 2");
+    root.addItem(sub);
+    root.addTextItem("Bauteil 1");
+    root.addTextItem("Bauteil 2");
+    root.addTextItem("Bauteil 3");
+    
+   
+    
+    sub.addTextItem("untergruppe");
+    
+    
+    
+    Tree t = new Tree();
+    t.addItem(root);
+    
+    
+    this.vPanelTreeTree.add(t);
 	
 	}
 }
