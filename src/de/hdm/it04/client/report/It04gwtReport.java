cdfae6e3293200feec7fc7698ae8d25eb097f499
Benjamin Schwab <Benny@192.168.1.4>
2015-06-26 10:43:04 -0700

    Report Generator Grundstruktur, 2. Entrypoint ergänzt

---
+++ src/de/hdm/it04/client/report/It04gwtReport.java
@@ -0,0 +1,22 @@
+package de.hdm.it04.client.report;
+
+import com.google.gwt.core.client.EntryPoint;
+//import com.google.gwt.core.client.GWT;
+//import com.google.gwt.user.client.ui.RootPanel;
+
+//import de.hdm.it04.client.gui.MainGUI;
+//import de.hdm.it04.client.service.It04gwtServiceClientReportImpl;
+
+
+	/**
+	 * Entry point classes define <code>onModuleLoad()</code>.
+	 */
+public class It04gwtReport implements EntryPoint {
+
+		@Override
+		public void onModuleLoad() {
+			// TODO Auto-generated method stub
+
+		}
+
+}
