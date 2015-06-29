ba1522afbc06bccb08bef2d0718b56c4c5e19094
ManuelVoelker <mv045@hdm-stuttgart.de>
2015-06-29 08:23:32 +0200

    Klasse Benutzer angelegt

---
+++ src/de/hdm/it04/shared/Benutzer.java
@@ -0,0 +1,21 @@
+package de.hdm.it04.shared;
+/**
+ * Die Klasse Benutzer repräsentiert einen Benutzer und ist im <code>shared</code> Package.
+ * Da die Klasse von <code>BusinessObject</code> erbt, 
+ * enthält sie auch deren Attribute.
+ */
+public class Benutzer extends BusinessObject {
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
