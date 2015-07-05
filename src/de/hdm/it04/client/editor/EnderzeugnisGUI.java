ec061370a152bce98241460f5a98b1ce8f4d0b38
FabianTschullik <fabian.tschullik@googlemail.com>
2015-07-05 13:17:37 +0200

    Enderzeugnis stuff

--- src/de/hdm/it04/client/editor/EnderzeugnisGUI.java
+++ src/de/hdm/it04/client/editor/EnderzeugnisGUI.java
@@ -10,6 +10,7 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
@@ -18,6 +19,7 @@ import com.google.gwt.user.client.ui.HTMLTable.Cell;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -25,6 +27,7 @@ import com.google.gwt.user.client.ui.Widget;
 
 import de.hdm.it04.client.service.It04gwtService;
 import de.hdm.it04.client.service.It04gwtServiceAsync;
+import de.hdm.it04.shared.Baugruppe;
 import de.hdm.it04.shared.Enderzeugnis;
 
 public class EnderzeugnisGUI {
@@ -45,12 +48,133 @@ public class EnderzeugnisGUI {
 	private HorizontalPanel hPanel = new HorizontalPanel();
 	public Enderzeugnis ez;
 	FlexTable enderzeugnisseTable = new FlexTable();
+	FlexTable baugruppeTable  = new FlexTable();
 	
 	It04gwtEditor user = new It04gwtEditor();
 	
 	
 
-	
+	public Widget showZuordnungsForm (Vector <Baugruppe> baugruppe){
+		
+		
+		/**
+		 * neuer HTML Bereich
+		 */
+		HTML topic = new HTML("<h2>Welche Baugruppe möchten Sie dem Enderzeugnis zuordnen?</h2>");
+		this.vPanel.add(topic);
+		
+		Button btnZuordnung = new Button("Jetzt zuordnen");
+		btnZuordnung.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				
+				sms.updateEnderzeugnis(ez, new AsyncCallback<Vector<Enderzeugnis>>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						new AlertGUI().load("Enderzeugnis wurde erfolgreich angelegt", "green");
+						
+					}
+
+					@Override
+					public void onSuccess(Vector<Enderzeugnis> result) {
+						new AlertGUI().load("Enderzeugnis wurde erfolgreich angelegt", "green");
+						
+					}
+				});
+			}
+		});
+		
+		this.vPanel.add(btnZuordnung);
+		
+		/**
+		 * Objekt der Klasse FlexTable erstellen und mit Spaltenueberschriften belegen
+		 */
+		baugruppeTable.setText(0,0,"ID");
+		baugruppeTable.setText(0,1,"Name");
+		baugruppeTable.setText(0,2,"Beschreibung");
+		baugruppeTable.setText(0,3,"Erstellt am");
+		baugruppeTable.setText(0,4,"Zuletzt geaendert am");
+		baugruppeTable.setText(0,5,"letzter Bearbeiter");
+		baugruppeTable.setText(0,6,"Zuordnen");
+		//bauteileTable.setText(0,7,"Delete");
+		
+		/**
+		 * Fuer jedes Bauteil werden die Tabellenspalten mit den Werten aus dem Vektor belegt
+		 */
+		for(int j=0; j < baugruppe.size(); j++ ){
+			
+			RadioButton rb = new RadioButton("Zuordnung");
+		
+			rb.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					
+					boolean checked = ((CheckBox) event.getSource()).getValue();
+					
+					if (checked == true){
+						
+						Cell cell = baugruppeTable.getCellForEvent(event);
+						
+						int rowIndex = cell.getRowIndex();
+						String id1 = baugruppeTable.getText(rowIndex, 0);
+						int id = Integer.parseInt(id1);
+						ez.setBaugruppe(id);
+					}
+				}
+			});
+			
+			/**
+			 * Formatiert Timestamp zu String
+			 */
+			/*Date d1 = new Date();
+			d1 = bauteile.elementAt(j).getErstellungsDatum();
+			String s1 = DateTimeFormat.getMediumDateTimeFormat().format(d1);*/
+			
+			
+			/**
+			 * Formatiert Timestamp zu String
+			 */
+			/*Date d2 = new Date();
+			d2 = bauteile.elementAt(j).getAenderungsDatum();
+			String s2 = DateTimeFormat.getMediumDateTimeFormat().format(d2);*/
+			
+		
+			/**
+			 * Konvertieren der Bauteil-Daten und befuellen der Tabelle
+			 */
+			baugruppeTable.setText(j+1, 0, Integer.toString(baugruppe.elementAt(j).getId()));
+			baugruppeTable.setText(j+1, 1, baugruppe.elementAt(j).getName());
+			baugruppeTable.setText(j+1, 2, baugruppe.elementAt(j).getBeschreibung());
+			//bauteileTable.setText(j+1, 3, s1);
+			//bauteileTable.setText(j+1, 4, s2);
+
+			baugruppeTable.setWidget(j+1, 6, rb);
+			
+			if(Integer.parseInt(baugruppeTable.getText(j+1, 0)) == ez.getBaugruppe()){
+				rb.setValue(true);
+			}
+			
+			
+			
+			/**
+			 * Verknuepfung zu style.css
+			 */
+			baugruppeTable.setCellPadding(6);
+			baugruppeTable.getRowFormatter().addStyleName(0,  "watchListHeader");
+			baugruppeTable.getCellFormatter().addStyleName(0,2, "watchListNumericColumn");
+			baugruppeTable.getCellFormatter().addStyleName(0,3, "watchListNumericColumn");	
+		}	
+		
+		/**
+		 * Bauteil-Tabelle zum Panel hinzugefuegen damit das Ganze auch angezeigt wird 
+		 */
+		this.vPanel.add(baugruppeTable);
+		
+		return vPanel;
+	}
 
 
 	public Widget suchen(){
