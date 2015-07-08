a17517dd770f2c54af52998d9ef5e20359a37bb5
Benjamin Schwab <Benny@10.60.51.195>
2015-07-07 20:32:30 -0700

    TL fertig

--- src/de/hdm/it04/shared/TeileListe.java
+++ src/de/hdm/it04/shared/TeileListe.java
@@ -14,18 +14,34 @@ public class TeileListe implements IsSerializable {
 	private int anzahl;
 	private int id;
 
+	/**
+	 * Getter-Methode, die die Anzahl der Teileliste zurueckgibt 
+	 * @return Die Anzahl als Integer
+	 */
 	public int getAnzahl() {
 		return anzahl;
 	}
 
+	/**
+	 * Setter-Methode die der Teileliste eine Anzahl hinzufuegt
+	 * @param anzahl
+	 */
 	public void setAnzahl(int anzahl) {
 		this.anzahl = anzahl;
 	}
 
+	/**
+	 * Getter-Methode die ID als Integer zurueckgibt
+	 * @return Die ID als Integer
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Setter-Methode die der Teileliste eine eindeutige ID hinzufuegt
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
