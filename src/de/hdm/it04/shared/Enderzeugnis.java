fd5377a9a63c8941373fb5a3b6d84cc90889b5e9
Benjamin Schwab <Benny@10.60.51.195>
2015-07-07 20:28:03 -0700

    EZ fertig

--- src/de/hdm/it04/shared/Enderzeugnis.java
+++ src/de/hdm/it04/shared/Enderzeugnis.java
@@ -12,18 +12,38 @@ public class Enderzeugnis extends Element {
 	private double Preis;
 	private int baugruppe;
 
+	/**
+	 * Getter-Methode, die den Preis eines Enderzeugnisses zurueckgibt
+	 * 
+	 * @return Der Preis als Double
+	 */
 	public double getPreis() {
 		return Preis;
 	}
 
+	/**
+	 * Setter-Methode, die dem Enderzeugnis einen Preis hinzufuegt
+	 * 
+	 * @param preis
+	 */
 	public void setPreis(double preis) {
 		this.Preis = preis;
 	}
 
+	/**
+	 * Getter-Methode, die die zugehoerige Baugruppe zurueckgibt
+	 * 
+	 * @return Die Baugruppe als Integer
+	 */
 	public int getBaugruppe() {
 		return baugruppe;
 	}
 
+	/**
+	 * Setter-Methode, die dem Enderzeugnis eine Baugruppe hinzufuegt
+	 * 
+	 * @param baugruppe
+	 */
 	public void setBaugruppe(int baugruppe) {
 		this.baugruppe = baugruppe;
 	}
